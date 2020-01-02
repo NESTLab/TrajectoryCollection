d001efcca89f57f8ab79f8c75f4f29a6abdddb70
NathalieMajcherczyk <nathalie.majcherczyk@gmail.com>
2020-01-02 09:37:55 -0500

    Tensorflow dataset preprocessing

--- bash-scripts/automate.sh
+++ bash-scripts/automate.sh
@@ -1,16 +1,45 @@
 #!/usr/bin/env bash
-#SBATCH -J argosRun
-#SBATCH -n 1
-#SBATCH -N 1
-#SBATCH -p short
-#SBATCH --mem 32G
-#SBATCH -C E5-2680
+# SBATCH -J argosRun
+# SBATCH -n 1
+# SBATCH -N 1
+# SBATCH -p short
+# SBATCH --mem 32G
+# SBATCH -C E5-2680
 
 # Stop execution after any error
 set -e
 
 # Useful variables
-JOB_LOC=run${3}/static
 BASE_LOC=$PWD
-DATADIR=$BASE_LOC/../../data #where you want your data to be stored
+DATADIR=$BASE_LOC/../data #where you want your data to be stored
 COUNT=0
+
+date +"%s"
+
+
+WORKDIR=$DATADIR
+CONFDIR=$WORKDIR/experiments/
+mkdir -p $CONFDIR
+cd $WORKDIR
+
+# Go through all the values for 
+for CONTROLLER in flocking #foraging khepera_obstacleavoidance 
+do
+   # Go through all the values
+   for NBROBOTS in 10 20 40
+   do
+      cd $WORKDIR
+      TIMESTAMP=`date "+%Y%m%d_%H%M%S"`
+      sed -e "s|TIMESTAMP|${TIMESTAMP}|;s|NBROBOTS|${NBROBOTS}|g" $BASE_LOC/experiments/${CONTROLLER}_batch.argos > $CONFDIR/exp_${CONTROLLER}_${NBROBOTS}.argos
+      cd $BASE_LOC
+      # # Execute program (this also writes files in work dir)
+      argos3 -l /dev/null -c $CONFDIR/exp_${CONTROLLER}_${NBROBOTS}.argos 2> "log_${CONTROLLER}_${NBROBOTS}_${TIMESTAMP}.txt"
+
+      echo $(( COUNT++ ))
+      echo "If you see this, experiment $COUNT/9 ${CONTROLLER}_${NBROBOTS}_${TIMESTAMP} is done"
+   done
+done
+
+mv *.dat $WORKDIR
+echo "Copying files"
+#rm -rf $CONFDIR
