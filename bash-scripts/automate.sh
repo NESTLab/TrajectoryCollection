34f01ce1b91453e5c7792b1bc746b77c8b36e594
NathalieMajcherczyk <nathalie.majcherczyk@gmail.com>
2020-10-10 18:27:00 -0400

    Edited parameters in experiment files for running on cluster and bash script for creating jobs

--- bash-scripts/automate.sh
+++ bash-scripts/automate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# SBATCH -J argosRun
-# SBATCH -n 1
-# SBATCH -N 1
-# SBATCH -p short
-# SBATCH --mem 32G
+#SBATCH -J argosRun
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -p short
+#SBATCH --mem 32G
 # SBATCH -C E5-2680
 
 # Stop execution after any error
@@ -22,26 +22,27 @@ CONFDIR=$WORKDIR/experiments/
 mkdir -p $CONFDIR
 cd $WORKDIR
 
+FOOD_FACTOR=2
+
 # Go through all the values for 
-for CONTROLLER in mixed_by_location #flocking flocking #mixed
+for CONTROLLER in khepera_obstacleavoidance flocking foraging mixed_by_location
 do
     for SEED in 2 #7 10
     do 
-      for NOISE in 0 #0.1 0.5
+      # Go through all the values
+      for NBROBOTS in 15 60 #40
       do
-         # Go through all the values
-         for NBROBOTS in 15 60 #40
-         do
-            cd $WORKDIR
-            TIMESTAMP=`date "+%Y%m%d_%H%M%S"`
-            sed -e "s|SEED|${SEED}|g;s|NOISE_WH|${NOISE}|g;s|NOISE_RAB|${NOISE}|g;s|TIMESTAMP|${TIMESTAMP}|g;s|NBROBOTS|${NBROBOTS}|g" $BASE_LOC/experiments/${CONTROLLER}_batch.argos > $CONFDIR/exp_${CONTROLLER}_${NBROBOTS}_${SEED}_${NOISE}_${TIMESTAMP}.argos
-            cd $BASE_LOC
-            # # Execute program (this also writes files in work dir)
-            argos3 -l /dev/null -c $CONFDIR/exp_${CONTROLLER}_${NBROBOTS}_${SEED}_${NOISE}_${TIMESTAMP}.argos 2> "log_${CONTROLLER}_${NBROBOTS}_${SEED}_${NOISE}_${TIMESTAMP}.txt"
+         FOOD_ITEMS=$((NBROBOTS*FOOD_FACTOR))
+         echo $FOOD_ITEMS
+         cd $WORKDIR
+         TIMESTAMP=`date "+%Y%m%d_%H%M%S"`
+         sed -e "s|SEED|${SEED}|g;s|TIMESTAMP|${TIMESTAMP}|g;s|FOOD_ITEMS|${FOOD_ITEMS}|g;s|NBROBOTS|${NBROBOTS}|g" $BASE_LOC/experiments/${CONTROLLER}_batch.argos > $CONFDIR/exp_${CONTROLLER}_${NBROBOTS}_${SEED}_${NOISE}_${TIMESTAMP}.argos
+         cd $BASE_LOC
+         # # Execute program (this also writes files in work dir)
+         argos3 -l /dev/null -c $CONFDIR/exp_${CONTROLLER}_${NBROBOTS}_${SEED}_${NOISE}_${TIMESTAMP}.argos 2> "log_${CONTROLLER}_${NBROBOTS}_${SEED}_${NOISE}_${TIMESTAMP}.txt"
 
-            echo $(( COUNT++ ))
-            echo "If you see this, experiment $COUNT/81 ${CONTROLLER}_${NBROBOTS}_${SEED}_${NOISE}_${TIMESTAMP} is done"
-         done
+         echo $(( COUNT++ ))
+         echo "If you see this, experiment $COUNT/81 ${CONTROLLER}_${NBROBOTS}_${SEED}_${NOISE}_${TIMESTAMP} is done"
       done
    done
 done
